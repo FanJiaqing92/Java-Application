539deaeb2dc4a2269aa89e3661dd5b41004ff7f6
Fanjiaqing92 <403559246@qq.com>
2019-01-08 22:39:21 +0800

    随意添加了一些东西

--- src/xml/dom4j/base/Dom4jNamespace.java
+++ src/xml/dom4j/base/Dom4jNamespace.java
@@ -2,9 +2,10 @@ package xml.dom4j.base;
 
 /**
  * Classname Dom4jNamespace
- * Description //TODO
+ * Description 尝试使用命名空间功能
  * Author Jiaqing Fan
  * Date 2018/11/13
  */
 public class Dom4jNamespace {
+
 }
